1564a05a0d772eca9138adacee3d99b694141791
Armaan Goyal <46539721+ag161920@users.noreply.github.com>
2023-06-14 13:35:47 -0500

    Update and rename func.py to func2.py

---
+++ func2.py
@@ -0,0 +1,9 @@
+def troll(N):
+    '''Self Explanatory.
+    Input: Number
+    Output: Flipped even/odd signifier.
+    '''
+    if N%2 == 0:
+        return "odd"
+    else:
+        return "even"
